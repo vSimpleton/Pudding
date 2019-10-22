7d60591a421533a843fd3ebd6a098380ed2ffb3e
vSimpleton <masm@luxuewen.com>
2019-10-22 16:48:37 +0800

    :zap: test

--- app/src/main/java/com/pomelo/pudding/utils/LogUtils.java
+++ app/src/main/java/com/pomelo/pudding/utils/LogUtils.java
@@ -9,46 +9,40 @@ import android.util.Log;
 
 public class LogUtils {
 
-    public static final int LEVEL_VERBOSE = 0; //日志输出级别V
-    public static final int LEVEL_DEBUG = 1; //日志输出级别D
-    public static final int LEVEL_INFO = 2; //日志输出级别I
-    public static final int LEVEL_WARN = 3; //日志输出级别W
-    public static final int LEVEL_ERROR = 4; //日志输出级别E
-
     private static String mTag = "youzi"; //日志输出时的TAG
-    private static int mDebuggable = 5; //是否允许输出log
+    private static final boolean DEBUG = true; //是否允许输出log
 
     /** 以级别为 d 的形式输出LOG */
     public static void v(String msg) {
-        if (mDebuggable >= LEVEL_VERBOSE) {
+        if (DEBUG) {
             Log.v(mTag, msg);
         }
     }
 
     /** 以级别为 d 的形式输出LOG */
     public static void d(String msg) {
-        if (mDebuggable >= LEVEL_DEBUG) {
+        if (DEBUG) {
             Log.d(mTag, msg);
         }
     }
 
     /** 以级别为 i 的形式输出LOG */
     public static void i(String msg) {
-        if (mDebuggable >= LEVEL_INFO) {
+        if (DEBUG) {
             Log.i(mTag, msg);
         }
     }
 
     /** 以级别为 w 的形式输出LOG */
     public static void w(String msg) {
-        if (mDebuggable >= LEVEL_WARN) {
+        if (DEBUG) {
             Log.w(mTag, msg);
         }
     }
 
     /** 以级别为 e 的形式输出LOG */
     public static void e(String msg) {
-        if (mDebuggable >= LEVEL_ERROR) {
+        if (DEBUG) {
             Log.e(mTag, msg);
         }
     }
